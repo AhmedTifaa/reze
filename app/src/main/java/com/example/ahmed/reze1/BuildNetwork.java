f8ad34663c28e2cc93a7b9d84dde54ca0a948a36
AhmedTifaa <ahmedtifaa53@gmail.com>
2018-04-15 12:15:14 +0200

    jjjj

--- app/src/main/java/com/example/ahmed/reze1/BuildNetwork.java
+++ app/src/main/java/com/example/ahmed/reze1/BuildNetwork.java
@@ -1,7 +1,9 @@
 package com.example.ahmed.reze1;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -10,6 +12,7 @@ import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -24,6 +27,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
@@ -37,12 +41,17 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.ahmed.reze1.helper.PrefManager;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.io.ByteArrayOutputStream;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+
+import lal.adhish.gifprogressbar.GifView;
 
 public class BuildNetwork extends AppCompatActivity {
 
@@ -50,6 +59,7 @@ public class BuildNetwork extends AppCompatActivity {
     private MyViewPagerAdapter myViewPagerAdapter;
     public int[] layouts;
     private Button btnNext;
+    private Button btnSkip;
     private PrefManager prefManager;
     private String fbname;
     private String fbpicurl;
@@ -66,7 +76,16 @@ public class BuildNetwork extends AppCompatActivity {
     public Spinner spinnerCarrer;
     public Spinner spinnerCity;
     public static final int PICK_IMAGE = 1;
-
+    public ListView listView ;
+    public ArrayList<String> StoreContacts ;
+    public ArrayAdapter<String> arrayAdapter ;
+    public Cursor cursor ;
+    public String name, phonenumber ;
+    public ProgressDialog progress;
+    public String result;
+    public ListView suggestlist;
+    public ArrayList<buildFriend>values;
+    public ArrayAdapter<String> stringArrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -83,11 +102,12 @@ public class BuildNetwork extends AppCompatActivity {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         }
 
-        setContentView(R.layout.activity_buildprofile);
+        setContentView(R.layout.activity_buildnetwork);
         Bundle inBundle = getIntent().getExtras();
+        user_id = inBundle.get("user_id").toString();
         viewPager = (ViewPager) findViewById(R.id.view_pager);
         requestQueue = Volley.newRequestQueue(this);
-        //btnSkip = (Button) findViewById(R.id.btn_skip);
+        btnSkip = (Button) findViewById(R.id.btn_skip);
         btnNext = (Button) findViewById(R.id.btn_next);
         //btnSkip.setVisibility(View.GONE);
 
@@ -101,12 +121,12 @@ public class BuildNetwork extends AppCompatActivity {
         myViewPagerAdapter = new MyViewPagerAdapter();
         viewPager.setAdapter(myViewPagerAdapter);
         viewPager.addOnPageChangeListener(viewPagerPageChangeListener);
-//        btnSkip.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//                //launchHomeScreen();
-//            }
-//        });
+        btnSkip.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -188,6 +208,85 @@ public class BuildNetwork extends AppCompatActivity {
 
             layoutInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
             View view = layoutInflater.inflate(layouts[position], container, false);
+            suggestlist = (ListView) view.findViewById(R.id.contacts_list);
+            values = new ArrayList<buildFriend>();
+
+            progress = new ProgressDialog(BuildNetwork.this);
+            progress.setTitle("Loading");
+            progress.setMessage("Wait while loading...");
+            progress.setCancelable(false); // disable dismiss by tapping outside of the dialog
+            progress.show();
+
+            StoreContacts = new ArrayList<String>();
+            cursor = getContentResolver().query(ContactsContract.CommonDataKinds.Phone.CONTENT_URI, null,null, null, null);
+            while (cursor.moveToNext()) {
+
+                name = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+
+                phonenumber = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER));
+                phonenumber = phonenumber.replaceAll("\\s+","");
+                StoreContacts.add(phonenumber);
+            }
+            cursor.close();
+            StringRequest request = new StringRequest(Request.Method.POST, "https://rezetopia.com/app/contacts.php", new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+                    progress.dismiss();
+                    //Toast.makeText(getBaseContext(),"test",Toast.LENGTH_LONG).show();
+
+                    Toast.makeText(getBaseContext(),response,Toast.LENGTH_LONG).show();
+
+                    try {
+                        JSONObject jsonObject;
+                        //jsonObject = new JSONObject(response);
+                        JSONArray jsonArray = new JSONArray(response);
+                        jsonArray.length();
+                        for (int i = 0;i<jsonArray.length();i++){
+                            jsonObject = new JSONObject(jsonArray.get(i).toString());
+                            values.add(new buildFriend(jsonObject.getString("name"),jsonObject.getString("id")));
+                        }
+                        contactAddapter contactAddapter = new contactAddapter(BuildNetwork.this,R.layout.item_friend,values);
+                        suggestlist.setAdapter(contactAddapter);
+                        //Toast.makeText(getBaseContext(),jsonArray.get(0).toString(),Toast.LENGTH_LONG).show();
+
+                       /* if(jsonObject.getString("msg").equals("done")){
+//                                Intent intent = new Intent(BuildProfile2.this,BuildNetwork.class);
+//                                intent.putExtra("user_id",user_id);
+//                                startActivity(intent);
+//                                finish();
+                        }
+                        else {
+                            //Toast.makeText(getBaseContext(),response.toString(),Toast.LENGTH_LONG).show();
+                        }*/
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                        Toast.makeText(getBaseContext(),e.getMessage().toString(),Toast.LENGTH_LONG).show();
+
+                    }
+                    //hideDialog();
+
+                }
+            }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+
+                }
+            }) {
+
+                @Override
+                protected Map<String, String> getParams() throws AuthFailureError {
+                    Map<String,String> parameters  = new HashMap<String, String>();
+                    for (int i = 0;i< StoreContacts.size();i++){
+                        parameters.put("contact"+i,StoreContacts.get(i));
+                    }
+
+
+                    return parameters;
+                }
+            };
+            requestQueue.add(request);
+            //new getFriendsContact().execute();
             container.addView(view);
             return view;
         }
