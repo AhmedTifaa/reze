8ae72f3c3645f3f58b64b9aa67742fe7eaf3c5a4
AhmedTifaa <ahmedtifaa53@gmail.com>
2018-04-22 14:09:34 +0200

    edit links

--- app/src/main/java/com/example/ahmed/reze1/PremiumProfileAccountFragment.java
+++ app/src/main/java/com/example/ahmed/reze1/PremiumProfileAccountFragment.java
@@ -9,5 +9,6 @@ public class PremiumProfileAccountFragment extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.premium_profile_account_fragment);
+
     }
 }
