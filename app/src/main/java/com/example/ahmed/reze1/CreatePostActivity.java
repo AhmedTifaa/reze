bf8992b4b25f2b93ba3a004d47a7f0b94bcd253a
AhmedTifaa <ahmedtifaa53@gmail.com>
2018-05-05 18:30:40 +0200

    posts profile

--- app/src/main/java/com/example/ahmed/reze1/CreatePostActivity.java
+++ app/src/main/java/com/example/ahmed/reze1/CreatePostActivity.java
@@ -14,6 +14,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.android.volley.AuthFailureError;
 import com.android.volley.Request;
@@ -60,6 +61,7 @@ public class CreatePostActivity extends AppCompatActivity implements View.OnClic
         postTextView = findViewById(R.id.new_post_desc);
         imageView = findViewById(R.id.new_post_image);
         postButton = findViewById(R.id.post_btn);
+
 
         imageView.setOnClickListener(this);
         postButton.setOnClickListener(this);
